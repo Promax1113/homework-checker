c4891f312041d2edff73b604b961e6b7bd7fb8b0
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-09-23 21:08:10 +0200

    Add incorrect password clause

--- data_processing/sumarizer.py
+++ data_processing/sumarizer.py
@@ -3,4 +3,5 @@ def make_summary(homework_list):
     index = 0
     for item in homework_list:
         index += 1
-        info = item.get(: {info['name']}\nCourse: {info['course']}\nDue: {info['due_date']}")
+        info = item.get_all()
+        print(f"{index}. {info['name']}\nCourse: {info['course']}\nDue: {info['due_date']}")
